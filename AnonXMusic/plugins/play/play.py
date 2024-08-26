d062720d4e6863583ad6e0e8f21b101d587ff3fa
Ahmetaffetmez <147531181+Ahmetaffetmez@users.noreply.github.com>
2024-08-26 11:53:47 +0300

    Update play.py
    Signed-off-by: Ahmetaffetmez <147531181+Ahmetaffetmez@users.noreply.github.com>

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -29,6 +29,7 @@ from config import BANNED_USERS, lyrical
     filters.command(
         [
             "play",
+            "vveke", 
             "veke", 
             "oynat", 
             "voynat", 
