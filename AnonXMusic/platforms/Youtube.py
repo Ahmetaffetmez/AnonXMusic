f6cc277fac4f7b916a1251273db9ecb7dd955fb3
Ahmetaffetmez <147531181+Ahmetaffetmez@users.noreply.github.com>
2024-12-31 19:27:38 +0200

    Update Youtube.py
    Signed-off-by: Ahmetaffetmez <147531181+Ahmetaffetmez@users.noreply.github.com>

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -10,8 +10,8 @@ from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from ArchMusic.utils.database import is_on_off
-from ArchMusic.utils.formatters import time_to_seconds
+from AnonXMusic.utils.database import is_on_off
+from AnonXMusic.utils.formatters import time_to_seconds
 
 
 def cookiefile():
